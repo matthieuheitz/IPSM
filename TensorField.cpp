1faba0a80a5c1f9c89d3980bde77be89b0c3358c
Matthieu Heitz <heitz.matthieu@gmail.com>
2015-12-11 18:50:11 +0100

    Add functions to create a grid tensor field
    Other functions to :
    Set/Get the field size
    Draw the tensor field as a vector field in an image
    Output to console the vector field

--- TensorField.cpp
+++ TensorField.cpp
@@ -1,6 +1,142 @@
-#include "tensorField.h"
+#include "TensorField.h"
+#include "math.h"
 
-tensorField::tensorField(QObject *parent) :
-    QObject(parent)
+#include <QPainter>
+#include <QPen>
+
+
+TensorField::TensorField(QSize fieldSize, QObject *parent) :
+    QObject(parent), mFieldSize(fieldSize)
 {
+    this->mData.resize(fieldSize.height());
+    for(int i=0 ; i < fieldSize.height() ; i++)
+    {
+        mData[i].resize(fieldSize.width());
+    }
+}
+
+
+void roundVector2D(QVector2D vec)
+{
+    vec.setX(round(vec.x()));
+    vec.setY(round(vec.y()));
+}
+
+// Get the tensor at index (i,j)
+QVector4D TensorField::getTensor(int i, int j)
+{
+    return mData[i][j];
+}
+
+// Set the tensor at index (i,j)
+void TensorField::setTensor(int i, int j, QVector4D tensor)
+{
+    this->mData[i][j] = tensor;
+}
+
+// Generate a grid basis function from an angle and
+// a vector's length (norm)
+void TensorField::fillGridBasisField(float theta, float l)
+{
+    float k = 2.0;
+    for(int i=0; i<this->mFieldSize.height() ; i++)
+    {
+        for(int j=0; j<this->mFieldSize.width() ; j++)
+        {
+            QVector4D tensor;
+            tensor.setX(cos(k*theta));
+            tensor.setY(sin(k*theta));
+            tensor.setZ(sin(k*theta));
+            tensor.setW(-cos(k*theta));
+            tensor*=l;
+            mData[i][j] = tensor;
+        }
+    }
+}
+
+// Generate a grid basis field from a 2D vector
+// Don't normalize the vector as this function
+// integrates the vector's norm in the tensor
+void TensorField::fillGridBasisField(QVector2D direction)
+{
+    float theta = atan(direction.y()/direction.x());
+    this->fillGridBasisField(theta, direction.length());
+}
+
+void TensorField::generateTensorField()
+{
+    qDebug()<<"Generate Tensor Field";
+    QVector2D vec(sqrt(3)/2.0f,1.0f/2.0f);
+//    QVector2D vec(1,0);
+    this->fillGridBasisField(vec);
+    this->exportTensorVectorsImage(true, true);
+}
+
+void TensorField::outputTensorField()
+{
+    for(int i=0; i<this->mFieldSize.height() ; i++)
+    {
+        for(int j=0; j<this->mFieldSize.width() ; j++)
+        {
+            qDebug()<<mData[i][j];
+        }
+        qDebug();
+    }
+}
+
+QPixmap TensorField::exportTensorVectorsImage(bool drawVector1, bool drawVector2,
+                                              QColor color1, QColor color2)
+{
+    int imageSize = 512;
+    QPixmap pixmap(imageSize,imageSize);
+    pixmap.fill();
+
+    QPainter painter(&pixmap);
+    QPen pen1(color1);
+    QPen pen2(color2);
+
+    float dv = imageSize/(float)this->mFieldSize.height();
+    float du = imageSize/(float)this->mFieldSize.width();
+    QVector2D origin(du/2.0f, dv/2.0f);
+
+    for(int i=0; i<this->mFieldSize.height() ; i++)
+    {
+        for(int j=0; j<this->mFieldSize.width() ; j++)
+        {
+            if(drawVector1)
+            {
+                painter.setPen(pen1);
+                QVector2D base = origin + QVector2D(i*dv, j*du);
+                QVector4D tensor = mData[i][j].normalized();
+                // TODO : Don't use the 2 columns of the tensor,
+                // but the 2 eigenvectors of the tensor instead.
+                QVector2D scaledVector(tensor.x(),tensor.y());
+                scaledVector.setX(scaledVector.x()*du/2.0f);
+                scaledVector.setY(scaledVector.y()*dv/2.0f);
+                QVector2D tip = base + scaledVector;
+                roundVector2D(base);
+                roundVector2D(tip);
+                painter.drawLine(base.x(),base.y(),tip.x(),tip.y());
+            }
+            if(drawVector2)
+            {
+                painter.setPen(pen2);
+                QVector2D base = origin + QVector2D(i*dv, j*du);
+                QVector4D tensor = mData[i][j].normalized();
+                QVector2D scaledVector(tensor.z(),tensor.w());
+                scaledVector.setX(scaledVector.x()*du/2.0f);
+                scaledVector.setY(scaledVector.y()*dv/2.0f);
+                QVector2D tip = base + scaledVector;
+                roundVector2D(base);
+                roundVector2D(tip);
+                painter.drawLine(base.x(),base.y(),tip.x(),tip.y());
+            }
+        }
+    }
+
+//    QLabel l;
+//    l.setPixmap(pixmap);
+//    l.show();
+    emit newTensorFieldImage(pixmap);
+    return pixmap;
 }
--- TensorField.h
+++ TensorField.h
@@ -2,16 +2,61 @@
 #define TENSORFIELD_H
 
 #include <QObject>
+#include <QImage>
+#include <QGenericMatrix>
+#include <QArrayData>
+#include <QVector>
+#include <QVector2D>
+#include <QVector4D>
+#include <QColor>
+#include <QPixmap>
+#include <QSize>
 
-class tensorField : public QObject
+class TensorField : public QObject
 {
     Q_OBJECT
 public:
-    explicit tensorField(QObject *parent = 0);
+    explicit TensorField(QSize fieldsize = QSize(256,256), QObject *parent = 0);
+
+    /** Getters and Setters **/
+
+    // Get the Tensor at index (i,j)
+    QVector4D getTensor(int i, int j);
+    // Set the Tensor at index (i,j)
+    void setTensor(int i, int j, QVector4D tensor);
+
+    // Get the tensor field size
+    QSize getFieldSize() {return this->mFieldSize;}
+    // Set the tensor field size
+    void setFieldSize(QSize fieldSize) {this->mFieldSize = fieldSize;}
+
+    /** General Use Functions */
+
+    // Creates a grid tensor field
+    void fillGridBasisField(QVector2D direction);
+    void fillGridBasisField(float theta, float l);
+
+    // Output the tensor field to QDebug
+    void outputTensorField();
+
+    // Display the tensor field with 2 vectors per point
+    QPixmap exportTensorVectorsImage(bool drawVector1 = true, bool drawVector2 = false,
+                                     QColor color1 = Qt::blue, QColor color2 = Qt::red);
+
+
 
 signals:
 
+    void newTensorFieldImage(QPixmap);
+
 public slots:
+
+    void generateTensorField();
+
+private:
+
+    QSize mFieldSize;
+    QVector<QVector<QVector4D> > mData;
 
 };
 
