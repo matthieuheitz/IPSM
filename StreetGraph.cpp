95842c451872a79f3af2133c5bfa30f4657a672f
Matthieu Heitz <heitz.matthieu@gmail.com>
2015-12-16 07:49:51 +0100

    ENH: Add Road and Node structures, and their map containers
    We use an integer ID to store the Roads and Nodes
    Add overload operator<< for Node, Road and QPointF

--- StreetGraph.cpp
+++ StreetGraph.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "StreetGraph.h"
 
 StreetGraph::StreetGraph(QPointF bottomLeft, QPointF topRight, TensorField *field, QObject *parent) :
@@ -11,5 +12,29 @@ StreetGraph::StreetGraph(QPointF bottomLeft, QPointF topRight, TensorField *fiel
     mLastRoadID = 0;
 }
 
+std::ostream& operator<<(std::ostream& out, const Road r)
 {
+    out<<"Road type = ";
+    out<<(r.type == Principal ? "Principal" : "Secondary");
+    out<<std::endl;
+    out<<"Path = ("<<r.segments.size()<<" points)"<<std::endl;
+    QVector<QPointF>::const_iterator it = r.segments.begin(), it_end = r.segments.end();
+    for(; it != it_end ; it++)
+    {
+        out<<"("<<it->x()<<","<<it->y()<<")"<<std::endl;
+    }
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const Node n)
+{
+    out<<"Node position = "<<n.position<<std::endl;
+    out<<"Number of connected roads = "<<n.connectedRoadIDs.size()<<std::endl;
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const QPointF p)
+{
+    out<<"("<<p.x()<<","<<p.y()<<")"<<std::endl;
+    return out;
 }
--- StreetGraph.h
+++ StreetGraph.h
@@ -2,8 +2,34 @@
 #define STREETGRAPH_H
 
 #include <QObject>
+#include <QPointF>
+#include <QSize>
+#include <QMap>
 
 #include "TensorField.h"
+
+struct Node;
+
+enum RoadType {
+    Principal,
+    Secondary
+};
+
+struct Road {
+    QVector<QPointF> segments;
+    int nodeID1;
+    int nodeID2;
+    RoadType type;
+};
+
+struct Node {
+    QPointF position;
+    QVector<int> connectedNodeIDs;
+    QVector<int> connectedRoadIDs;
+};
+
+typedef QMap<int,Node>::iterator NodeMapIterator;
+typedef QMap<int,Road>::iterator RoadMapIterator;
 
 class StreetGraph : public QObject
 {
@@ -21,12 +47,27 @@ private:
 
     // Tensor field
     TensorField * mTensorField;
+    // Container for nodes
+    QMap<int,Node> mNodes;
+    // Container for roads
+    QMap<int,Road> mRoads;
     // Height and width of the region
     QSizeF mRegionSize;
     // Coordinates of the bottom left point
     QPointF mBottomLeft;
     // Coordinates of the top right point
     QPointF mTopRight;
+    // Last IDs for Nodes and Roads
+    int mLastNodeID;
+    int mLastRoadID;
+
 };
+
+// Overloads writing Road to std stream
+std::ostream& operator<<(std::ostream& out, const Road r);
+// Overloads writing Node to std stream
+std::ostream& operator<<(std::ostream& out, const Node n);
+// Overloads writing QPointF to std stream
+std::ostream& operator<<(std::ostream& out, const QPointF p);
 
 #endif // STREETGRAPH_H
