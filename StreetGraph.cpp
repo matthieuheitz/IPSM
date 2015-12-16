018c1a83cb4e96a35181b37b0c63c49ab7be8c5d
Matthieu Heitz <heitz.matthieu@gmail.com>
2015-12-16 07:42:51 +0100

    ENH: Add constructor with region limits and tensor field

--- StreetGraph.cpp
+++ StreetGraph.cpp
@@ -1,6 +1,15 @@
 #include "StreetGraph.h"
 
-StreetGraph::StreetGraph(QObject *parent) :
-    QObject(parent)
+StreetGraph::StreetGraph(QPointF bottomLeft, QPointF topRight, TensorField *field, QObject *parent) :
+    QObject(parent), mTensorField(field), mBottomLeft(bottomLeft), mTopRight(topRight)
+{
+    mRegionSize.rwidth() = (topRight-bottomLeft).x();
+    mRegionSize.rheight() = (topRight-bottomLeft).y();
+    qDebug()<<"Region size = "<<mRegionSize.width()<<", "
+             <<mRegionSize.height();
+    mLastNodeID = 0;
+    mLastRoadID = 0;
+}
+
 {
 }
--- StreetGraph.h
+++ StreetGraph.h
@@ -3,16 +3,30 @@
 
 #include <QObject>
 
+#include "TensorField.h"
+
 class StreetGraph : public QObject
 {
     Q_OBJECT
 public:
-    explicit StreetGraph(QObject *parent = 0);
+    // Construct a graph within limits passed
+    //
+    explicit StreetGraph(QPointF bottomLeft, QPointF topRight, TensorField * field, QObject *parent = 0);
 
 signals:
 
 public slots:
 
+private:
+
+    // Tensor field
+    TensorField * mTensorField;
+    // Height and width of the region
+    QSizeF mRegionSize;
+    // Coordinates of the bottom left point
+    QPointF mBottomLeft;
+    // Coordinates of the top right point
+    QPointF mTopRight;
 };
 
 #endif // STREETGRAPH_H
